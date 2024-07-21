02c404eb831030f2b6c86b737fb5d01212897036
fredmarmillod <fmarmillod@gmail.com>
2024-07-21 12:18:40 +0200

    cleanup test configuration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,29 +8,10 @@ use Pdfapiio\PdfapiLaravel\PdfapiLaravelServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Pdfapiio\\PdfapiLaravel\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             PdfapiLaravelServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_pdfapi-laravel_table.php.stub';
-        $migration->up();
-        */
     }
 }
