0dfbd884cd5ae7001ccd30c1e678f21ed220025f
fredmarmillod <fredmarmillod@users.noreply.github.com>
2024-07-21 10:20:10 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Pdfapiio\PdfapiLaravel\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Pdfapiio\PdfapiLaravel\PdfapiLaravelServiceProvider;
 
